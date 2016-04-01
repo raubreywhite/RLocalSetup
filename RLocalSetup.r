d2fe918e7cd0e3e03a10b527a02e75186e6c1a21
Richard White <r.aubrey.white@gmail.com>
2016-04-01 14:08:14 +0200

    Update RLocalSetup.r

--- RLocalSetup.r
+++ RLocalSetup.r
@@ -157,7 +157,7 @@ CreatePackage <- function(name="test",depends=NULL,imports=NULL){
   file.copy(file, paste0("reports_skeleton/references.bib"), overwrite=TRUE)
 
   packrat::off()
-  erase.file(".Rprofile")
+  file.remove(".Rprofile")
   
   write("Version: 1.0\n\nRestoreWorkspace: No\nSaveWorkspace: No\nAlwaysSaveHistory: No\n\nEnableCodeIndexing: Yes\nUseSpacesForTab: Yes\nNumSpacesForTab: 2\nEncoding: ISO8859-1\n\n\nRnwWeave: Sweave\nLaTeX: pdfLaTeX",file=paste0(name,".Rproj"))
   write(".Rproj.user\n.Rhistory\n.RData\nresults_temp/\nresults_final/\ndata_temp/\ndata_clean/\nreports_formatted/\npres_formatted/\npackrat/",file=".gitignore")
