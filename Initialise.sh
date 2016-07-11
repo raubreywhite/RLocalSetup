e93bd67e9da547234376fb083c3c515126f355c8
Richard White <r.aubrey.white@gmail.com>
2016-07-11 23:07:39 +0200

    Update Initialise.sh

--- Initialise.sh
+++ Initialise.sh
@@ -1,6 +1,6 @@
 docker run --rm \
  -v /media/FHI/Felles/Prosjekter/Dashboards/projects/X/:/src \
  -v /home/shiny/shinyapps_prod/log/data/:/log/ \
- dashboardsfhi/hadleyverse Rscript /src/Run.R
+ dashboardsfhi/hadleyverse Rscript /src/Initialise.R
 
 # -v /home/shiny/shinyapps_prod/X/:/app \
